9b94249fb0112443340792019d06da7e0b503f18
sairamsreejith0 <30547601+sairamsreejith0@users.noreply.github.com>
2020-02-19 14:20:22 +0530

    Add files via upload

---
+++ cleancode-hometask3/src/main/java/com/epam/ht3/cleancode_hometask3/Main.java
@@ -0,0 +1,14 @@
+package com.epam.ht3.cleancode_hometask3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	Interest.calculateInterest();
+        Construction.costEvaluator();
+    }
+}
